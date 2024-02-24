8a96b79fcc7de5943247d0efc5d10bdd5803b5ab
tedddby <katotzu@gmail.com>
2024-02-24 15:29:59 -0800

    Update pricing.js

--- controllers/pricing.js
+++ controllers/pricing.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql");
 const env = require("dotenv");
-const fs = require("fs");
 
 env.config({
   path:"./sec.env"
@@ -17,10 +16,8 @@ const db = mysql.createConnection({
 db.connect((error) => {
   if(error){
   console.log(error);
-  fs.writeFileSync('./log_ted.txt', error.toString())
   }else{
   console.log("DB Connected");
-  fs.writeFileSync('./log_ted', "DB Connected");
   }
 });
 
@@ -28,22 +25,17 @@ const promisify = f => (...args) => new Promise((a,b)=>f(...args, (err, res) => 
 
 const setPrice = (service, model, callback) => {
 
-  fs.writeFileSync('./log_ted2.txt', model);
-
   db.query("SELECT * FROM "+service.replace(/ /g,"_")+" WHERE model = ?", [model.toUpperCase()], (error, result) => {
     if(error){
-      fs.writeFileSync('./log_ted3.txt', error.toString());
       callback(20000);
     }else{
       if(result && result != ""){
-        fs.writeFileSync('./log_ted3.txt', result[0].price);
         if(result[0].model == model){
           callback(result[0].price);
         }else{
           callback(10000);
         }
       }else{
-        fs.writeFileSync('./log_ted3.txt', "nothing");
         callback(30000);
       }
     }
